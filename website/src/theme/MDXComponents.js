4d855056d41b65f7bb635bf3910daaf1dc6ee181
benjokoren <ben@benjo.co.il>
2022-07-05 10:46:49 +0300

    new hide me skippy component

--- website/src/theme/MDXComponents.js
+++ website/src/theme/MDXComponents.js
@@ -4,6 +4,7 @@ import MDXComponents from '@theme-original/MDXComponents';
 import ImageSlider from '@site/src/components/image-slider/image-slider';
 import ImageBoxModal from '@site/src/components/image-box-modal/image-box-modal';
 import VideoBox from '@site/src/components/video-box/video-box';
+import HideMeSkippy from '@site/src/components/hide-me/hide-me';
 
 export default {
   // Re-use the default mapping
@@ -13,4 +14,5 @@ export default {
   ImageSlider: ImageSlider,
   ImageBoxModal: ImageBoxModal,
   VideoBox: VideoBox,
+  HideMeSkippy: HideMeSkippy,
 };
